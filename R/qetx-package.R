588e42199ccc3683fc6e2da97810e07a121932ff
EDiLD <szoe8822@uni-landau.de>
2013-10-09 17:31:22 +0200

    added so4 data

---
+++ R/qetx-package.R
@@ -0,0 +1,13 @@
+#' @name qetx-package
+#' @aliases qetx
+#' @docType package
+#' @title Quantitative Ecotoxicology with R
+#' @author Eduard Szoecs \email{szoe8822@@uni-landau.de}
+#' @keywords package
+NULL
+#' Sulfate concentrations (mg/L) measured during a routine water quality survey 
+#' of the Savannah River (South Carolina).
+#' @name so4
+#' @docType data
+#' @keywords data
+NULL
