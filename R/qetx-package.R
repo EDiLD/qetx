10ffd4a789b0295376187e2577596267c88fe91a
EDiLD <szoe8822@uni-landau.de>
2013-10-16 22:03:00 +0200

    added abu data

--- R/qetx-package.R
+++ R/qetx-package.R
@@ -7,8 +7,17 @@
 NULL
 #' Sulfate concentrations (mg/L) measured during a routine water quality survey 
 #' of the Savannah River (South Carolina). 
-#' @return A vector with measurements.
+#' @usage data(so4)
+#' @format A vector with 21 measurements.
 #' @name so4
+#' @docType data
+#' @keywords data
+NULL
+#' Species sampled at eight sites around an outfall. 
+#' Site locations are designated as kilometers upstream (-) or downstream (+) of the outfall.
+#' @usage data(abu)
+#' @format A data.frame with 8 observations of abundance of 10 Species.
+#' @name abbu
 #' @docType data
 #' @keywords data
 NULL
