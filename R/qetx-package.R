2f7aed52c5f4cccc45f40ce8921fa3ddecf5acde
EDiLD <szoe8822@uni-landau.de>
2013-10-23 22:38:57 +0200

    added oil dataset

--- R/qetx-package.R
+++ R/qetx-package.R
@@ -48,3 +48,48 @@ NULL
 #' @docType data
 #' @keywords data
 NULL
+#' Benthic community data around a offshore installation
+#' @description Samples were collected at sampling stations with varying distanced from installation along the transects.
+#' For environmental (and distance) data see \code{\link{oil_env}}.
+#' @usage data(oil)
+#' @format A dataframe of observations of 50 species at 20 sites.
+#' @source Data was retrieved from the UK Benthos Database,Oil & Gas UK , http://www.oilandgasuk.co.uk/knowledgecentre/uk_benthos_database.cfm
+#' @name oil
+#' @docType data
+#' @keywords data
+#' @rdname oil
+#' @seealso \code{\link{oil_env}}
+NULL
+#' Corresponding environmental data to oil data set
+#' @usage data(oil_env)
+#' @format A dataframe of 20 observations of 22 variables:
+#' \describe{
+#' \item{distance}{distance from installation in m.}
+#' \item{site}{A sampling site identifier.}
+#' \item{silt_clay}{Silt/clay content of the sediment as percent.}
+#' \item{organic}{Organic content of the sedimentas percent.}
+#' \item{tot_hc_gc}{Total Hydrocarbon determined by gas chromatography.}
+#' \item{naph_ult}{Naphthalenes (via ultrasonification).}
+#' \item{phen_ult}{Phenanthrenes (via ultrasonification).}
+#' \item{dibenz_ult}{Diabenzothiophenes (via ultrasonification).}
+#' \item{pah_4r_202}{Four ring compounds (mass 202).}
+#' \item{pah_4r_228}{Four ring compounds (mass 228).}
+#' \item{pah_5r_252}{Five ring compounds (mass 252).}
+#' \item{pah_6r_276}{Six ring compounds (mass 2575).}
+#' \item{ba_tot}{Total Barium concentration.}
+#' \item{cd}{Cadmium.}
+#' \item{cr}{Chromium.}
+#' \item{cu}{Copper.}
+#' \item{ni}{Nickel.}
+#' \item{pb}{Lead.}
+#' \item{va}{Vanadium.}
+#' \item{zn}{Zinc.}
+#' \item{fe}{Iron.}
+#' \item{hg}{mercury}
+#' }
+#' @source Data was retrieved from the UK Benthos Database,Oil & Gas UK , http://www.oilandgasuk.co.uk/knowledgecentre/uk_benthos_database.cfm
+#' @name oil_env
+#' @docType data
+#' @keywords data
+#' @seealso \code{\link{oil}}
+NULL
